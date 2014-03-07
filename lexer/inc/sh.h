b2529ef9050799062cd85c2bae58e4e070fec9ca
Conrad Kleinespel <conradk@conradk.com>
2014-03-07 01:57:14 +0100

    fix parse

--- lexer/inc/sh.h
+++ lexer/inc/sh.h
@@ -6,7 +6,7 @@
 /*   By: ckleines <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/28 03:59:39 by ckleines          #+#    #+#             */
-/*   Updated: 2014/03/06 17:25:40 by ckleines         ###   ########.fr       */
+/*   Updated: 2014/03/07 01:54:59 by ckleines         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,40 +24,48 @@ typedef struct					s_sh_parent_check_corr
 	int			type;
 }								t_sh_parent_check_corr;
 
-int		lexer_callback_subcommand_dollar(t_am *am);
-int		lexer_callback_subcommand_backtick(t_am *am);
-int		sh_token_is_string(t_am_token *t);
-int		lexer_callback_single_quote_string_content(t_am *am);
-t_am	*lexer_automaton_single_quote_string(void);
-int		lexer_callback_double_quote_string_content(t_am *am);
-t_am	*lexer_automaton_double_quote_string(void);
-int		is_not_meaningful(char c);
-int		lexer_callback_string(t_am *am);
-t_am	*lexer_automaton_string(void);
-t_am	*lexer_automaton_command_line_token(void);
-int		am_lex(t_am_maker maker, const char *source, t_ckl *tokens);
-void	sh_remove_useless_tokens(t_ckl *tokens);
-int		sh_type_is_op(int type);
-int		sh_token_is_quoted_string(t_am_token *t);
-int		sh_token_is_string(t_am_token *t);
-void	sh_concat_strings(t_ckl *t);
-void	sh_remove_whitespace(t_ckl *tokens);
-int		sh_lex(const char *src, t_ckl *tokens);
-void	sh_replace_redirections(t_ckl *t, const char *base);
-void	sh_print_tokens(t_ckl *tokens);
-int		sh_build_tree(t_ckl *tokens, t_ckbt *tree);
-void	sh_infile(t_ckl *tokens, t_ckl_item *start, const t_cks file,
-			const char *base);
-void	sh_heredoc(t_ckl *tokens, t_ckl_item *start, const t_cks boundary,
-			const char *base);
-void	sh_replace_redirections_in(t_ckl *t, t_ckl_item *start,
-			const char *base);
-void	sh_outfile(t_ckl *tokens, t_ckl_item *end, const t_cks file,
-			const char *base);
-void	sh_outfile_append(t_ckl *tokens, t_ckl_item *end,
-			const t_cks boundary, const char *base);
-void	sh_replace_redirections_out(t_ckl *t, t_ckl_item *start,
-			t_ckl_item *end, const char *base);
-int		sh_parse(const char *src, t_ckbt *tree, const char *basepath);
+int			lexer_callback_subcommand_dollar(t_am *am);
+int			lexer_callback_subcommand_backtick(t_am *am);
+int			sh_token_is_string(t_am_token *t);
+int			lexer_callback_single_quote_string_content(t_am *am);
+t_am		*lexer_automaton_single_quote_string(void);
+int			lexer_callback_double_quote_string_content(t_am *am);
+t_am		*lexer_automaton_double_quote_string(void);
+int			is_not_meaningful(char c);
+int			lexer_callback_string(t_am *am);
+t_am		*lexer_automaton_string(void);
+t_am		*lexer_automaton_command_line_token(void);
+int			am_lex(t_am_maker maker, const char *source, t_ckl *tokens);
+void		sh_remove_useless_tokens(t_ckl *tokens);
+int			sh_type_is_op(int type);
+int			sh_token_is_quoted_string(t_am_token *t);
+int			sh_token_is_string(t_am_token *t);
+void		sh_concat_strings(t_ckl *t);
+void		sh_remove_whitespace(t_ckl *tokens);
+int			sh_lex(const char *src, t_ckl *tokens);
+void		sh_replace_redirections(t_ckl *t, const char *base);
+void		sh_print_tokens(t_ckl *tokens);
+int			sh_build_tree(t_ckl *tokens, t_ckbt *tree);
+void		sh_infile(t_ckl *tokens, t_ckl_item *start, const t_cks file,
+				const char *base);
+void		sh_heredoc(t_ckl *tokens, t_ckl_item *start, const t_cks boundary,
+				const char *base);
+void		sh_replace_redirections_in(t_ckl *t, t_ckl_item *start,
+				const char *base);
+void		sh_outfile(t_ckl *tokens, t_ckl_item *end, const t_cks file,
+				const char *base);
+void		sh_outfile_append(t_ckl *tokens, t_ckl_item *end,
+				const t_cks boundary, const char *base);
+void		sh_replace_redirections_out(t_ckl *t, t_ckl_item *start,
+				t_ckl_item *end, const char *base);
+int			sh_parse(const char *src, t_ckbt *tree, const char *basepath);
 
+int			sh_parse_cmd_line(t_ckl *tokens, t_ckbt *tree, t_ckbt_node **root);
+int			sh_parse_seq_cmd(t_ckl *tokens, t_ckbt *tree, t_ckbt_node **root);
+int			sh_parse_log_cmd(t_ckl *tokens, t_ckbt *tree, t_ckbt_node **root);
+int			sh_parse_pipe_cmd(t_ckl *tokens, t_ckbt *tree, t_ckbt_node **root);
+int			sh_parse_cmd(t_ckl *tokens, t_ckbt *tree, t_ckbt_node **root);
+t_ckbt_node	*sh_new_node_logic(t_ckbt *tree, t_sh_token_type type);
+t_ckbt_node	*sh_new_node_semicolon(t_ckbt *tree);
+t_ckbt_node	*sh_new_node_pipe(t_ckbt *tree);
 #endif
--- lexer/src/sh_build_tree.c
+++ lexer/src/sh_build_tree.c
@@ -6,7 +6,7 @@
 /*   By: ckleines <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/06 17:31:27 by ckleines          #+#    #+#             */
-/*   Updated: 2014/03/07 01:48:19 by ckleines         ###   ########.fr       */
+/*   Updated: 2014/03/07 01:56:50 by ckleines         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,15 @@
 **
 ** cmd := string [ string ]*
 */
+
+t_ckbt_node	*sh_new_node_pipe(t_ckbt *tree)
+{
+	t_sh_command	cmd;
+
+	cmd.type = SH_COMMAND_TYPE_PIPE;
+	cmd.argv = NULL;
+	return (ckbt_new_node(tree, &cmd));
+}
 
 t_ckbt_node	*sh_new_node_semicolon(t_ckbt *tree)
 {
@@ -49,7 +58,38 @@ t_ckbt_node	*sh_new_node_logic(t_ckbt *tree, t_sh_token_type type)
 	return (ckbt_new_node(tree, &cmd));
 }
 
-int			sh_parse_log_cmd(t_ckl *tokens, t_ckbt_node **root)
+int			sh_parse_cmd(t_ckl *tokens, t_ckbt *tree, t_ckbt_node **root)
+{
+	tokens = NULL;
+	tree = NULL;
+	root = NULL;
+	/* FILL THIS IN */
+	return (0);
+}
+
+int			sh_parse_pipe_cmd(t_ckl *tokens, t_ckbt *tree, t_ckbt_node **root)
+{
+	t_am_token		*tok;
+	t_ckbt_node		*node;
+
+	if (tokens->first == NULL)
+		return (1);
+	node = NULL;
+	if (sh_parse_cmd(tokens, tree, &node) == 1)
+	{
+		tok = &ckl_data(t_am_token, tokens->first);
+		if (tok->type != SH_TOKEN_TYPE_PIPE)
+			return (1);
+		*root = sh_new_node_pipe(tree);
+		(*root)->left = node;
+		ckl_withdraw(tokens, tokens->first);
+		return (sh_parse_pipe_cmd(tokens, tree, &(*root)->right));
+	}
+	*root = node;
+	return (0);
+}
+
+int			sh_parse_log_cmd(t_ckl *tokens, t_ckbt *tree, t_ckbt_node **root)
 {
 	t_am_token		*tok;
 	t_ckbt_node		*node;
@@ -65,7 +105,7 @@ int			sh_parse_log_cmd(t_ckl *tokens, t_ckbt_node **root)
 			return (1);
 		*root = sh_new_node_logic(tree, tok->type);
 		(*root)->left = node;
-		ckl_withdraw(tokens->first);
+		ckl_withdraw(tokens, tokens->first);
 		return (sh_parse_log_cmd(tokens, tree, &(*root)->right));
 	}
 	*root = node;
@@ -87,7 +127,7 @@ int			sh_parse_seq_cmd(t_ckl *tokens, t_ckbt *tree, t_ckbt_node **root)
 			return (1);
 		*root = sh_new_node_semicolon(tree);
 		(*root)->left = node;
-		ckl_withdraw(tokens->first);
+		ckl_withdraw(tokens, tokens->first);
 		return (sh_parse_cmd_line(tokens, tree, &(*root)->right));
 	}
 	*root = node;
@@ -98,7 +138,7 @@ int			sh_parse_cmd_line(t_ckl *tokens, t_ckbt *tree, t_ckbt_node **root)
 {
 	if (tokens->first == NULL)
 		return (0);
-	return (sh_parse_non_empty_cmd_line(tokens, tree, root));
+	return (sh_parse_seq_cmd(tokens, tree, root));
 }
 
 int			sh_build_tree(t_ckl *tokens, t_ckbt *tree)
