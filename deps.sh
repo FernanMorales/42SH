0fe4ec4f59e3778aa4dfefc9a2a5902f0b42958f
Conrad KLEINESPEL <ckleines@e1r6p21.42.fr>
2014-03-18 14:34:30 +0100

    fix deps

--- deps.sh
+++ deps.sh
@@ -1,2 +1,2 @@
 # push
-for i in $(find . -type d -name 'libck*' | grep -v '^./.git'); do cd $i; git branch tmp; git checkout tmp; git add -A; git commit -m 'deps'; git checkout master; git merge tmp; git branch -D tmp; git push origin master; cd -; done
+for i in $(find . -type d -name 'libck*' | grep -v '^./.git'); do cd $i; git branch tmp; git checkout tmp; git add -A; git commit -m 'deps'; git checkout master; git merge tmp; git branch -D tmp; git push origin master; git pull origin master; cd -; done
