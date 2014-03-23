e6954ad0aa47c3fea1b76e05c04cbd01c5b023fd
Conrad KLEINESPEL <ckleines@e1r6p16.42.fr>
2014-03-23 15:29:28 +0100

    added headers everywhere :P

--- launcher/src/sh_init_env.c
+++ launcher/src/sh_init_env.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sh_init_env.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ckleines <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/03/23 15:20:54 by ckleines          #+#    #+#             */
+/*   Updated: 2014/03/23 15:21:43 by ckleines         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "sh42.h"
 
 int		sh_init_env(t_sh_env *env, int argc, const char **argv)
@@ -8,8 +20,6 @@ int		sh_init_env(t_sh_env *env, int argc, const char **argv)
 		return (1);
 	if ((tty = ttyname(0)) == NULL
 		|| (env->tty_fd = open(tty, O_WRONLY) == -1))
-		return (1);
-	if (sh_init_env_get_base(argv[0], &env->base))
 		return (1);
 	env->last_ret = 0;
 	return (0);
