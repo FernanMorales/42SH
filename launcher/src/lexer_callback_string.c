26d484003e428e22a3e5aafb351ba173ca45147c
Conrad Kleinespel <conradk@conradk.com>
2014-03-24 11:14:41 +0100

    norme

--- launcher/src/lexer_callback_string.c
+++ launcher/src/lexer_callback_string.c
@@ -13,6 +13,32 @@
 #include "am.h"
 #include "sh.h"
 
+void	st_concat_next(t_cks *orig, t_cks *computed, size_t *i, t_am *am)
+{
+	*orig = cks_append_len(*orig, am->source + *i, 2);
+	*computed = cks_append_len(*computed, am->source + *i + 1, 1);
+	*i += 2;
+}
+
+void	st_concat_curr(t_cks *orig, t_cks *computed, size_t *i, t_am *am)
+{
+	*orig = cks_append_len(*orig, am->source + *i, 1);
+	*computed = cks_append_len(*computed, am->source + *i, 1);
+	(*i)++;
+}
+
+int		st_add_or_fail(t_cks orig, t_cks computed, t_am *am)
+{
+	if (cks_len(orig) > 0)
+	{
+		am_add_token(am, orig, computed);
+		return (1);
+	}
+	cks_free(orig);
+	cks_free(computed);
+	return (0);
+}
+
 int		lexer_callback_string(t_am *am)
 {
 	size_t		i;
@@ -25,17 +51,9 @@ int		lexer_callback_string(t_am *am)
 	while (i < cks_len(am->source) && is_not_meaningful(am->source[i]))
 	{
 		if (i + 1 < cks_len(am->source) && am->source[i] == '\\')
-		{
-			orig = cks_append_len(orig, am->source + i, 2);
-			computed = cks_append_len(computed, am->source + i + 1, 1);
-			i += 2;
-		}
+			st_concat_next(&orig, &computed, &i, am);
 		else if (am->source[i] != '\\')
-		{
-			orig = cks_append_len(orig, am->source + i, 1);
-			computed = cks_append_len(computed, am->source + i, 1);
-			i++;
-		}
+			st_concat_curr(&orig, &computed, &i, am);
 		else
 		{
 			cks_free(orig);
@@ -43,12 +61,5 @@ int		lexer_callback_string(t_am *am)
 			return (0);
 		}
 	}
-	if (cks_len(orig) > 0)
-	{
-		am_add_token(am, orig, computed);
-		return (1);
-	}
-	cks_free(orig);
-	cks_free(computed);
-	return (0);
+	return (st_add_or_fail(orig, computed, am));
 }
