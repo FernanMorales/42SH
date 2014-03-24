ad435feb73fca8e3f0939718ef9a693f20764887
Conrad Kleinespel <conradk@conradk.com>
2014-03-24 19:10:18 +0100

    added removing backslahes from single quotes strings

--- launcher/src/sh_preprocess_cmd.c
+++ launcher/src/sh_preprocess_cmd.c
@@ -1,11 +1,50 @@
 #include "sh42.h"
 
+int		st_preprocess_home(t_sh_env *env, t_cks *arg)
+{
+	const char		*home;
+	t_cks			ns;
+
+	if ((*arg)[0] == '~' && ((*arg)[1] == '/' || !(*arg)[1]))
+	{
+		if ((home = getenv("HOME")) == NULL)
+			return (1);
+		ns = cks_sub(*arg, 1);
+		cks_free(*arg);
+		ns = cks_prepend(ns, home);
+		*arg = ns;
+	}
+	return (0);
+	env = NULL;
+}
+
+void	st_remove_backslashes(t_cks *arg)
+{
+	t_cks		ns;
+	t_cks		that;
+
+	that = *arg;
+	ns = cks_new("");
+	while (*that)
+	{
+		if (*that == '\\' && *(that + 1))
+		{
+			ns = cks_append_len(ns, that + 1, 1);
+			that += 2;
+		}
+		else if (*that != '\\')
+			ns = cks_append_len(ns, that++, 1);
+		else
+			that++;
+	}
+	cks_free(*arg);
+	*arg = ns;
+}
+
 int		sh_preprocess_cmd(t_sh_env *env, t_sh_command *cmd)
 {
 	t_ckl_item		*item;
 	t_cks			*arg;
-	t_cks			ns;
-	const char		*home;
 
 	if (cmd->argv == NULL)
 		return (2);
@@ -13,17 +52,10 @@ int		sh_preprocess_cmd(t_sh_env *env, t_sh_command *cmd)
 	while (item)
 	{
 		arg = &ckl_data(t_cks, item);
-		if ((*arg)[0] == '~' && (*arg)[1] == '/')
-		{
-			if ((home = getenv("HOME")) == NULL)
-				return (1);
-			ns = cks_sub(*arg, 1);
-			cks_free(*arg);
-			ns = cks_prepend(ns, home);
-			*arg = ns;
-		}
+		if (st_preprocess_home(env, arg))
+			return (1);
+		st_remove_backslashes(arg);
 		item = item->next;
 	}
 	return (0);
-	env = NULL;
 }
