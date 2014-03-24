c9462e510974bc803f7c89f004ad7bef96626aa2
Conrad Kleinespel <conradk@conradk.com>
2014-03-24 10:35:57 +0100

    fix norme

--- launcher/src/am_finder_automaton.c
+++ launcher/src/am_finder_automaton.c
@@ -15,12 +15,39 @@
 #include "ckl.h"
 #include "ckstd.h"
 
+void	st_join_tokens(t_am *am, t_am *nam, t_am_token *joined_token)
+{
+	t_am_token	*curr_token;
+	t_ckl_item	*item;
+
+	joined_token->value_computed = cks_new("");
+	joined_token->value_orig = cks_new("");
+	joined_token->type = am->curr_finder->token_type;
+	item = nam->tokens->first;
+	while (item)
+	{
+		curr_token = &ckl_data(t_am_token, item);
+		joined_token->value_computed = cks_append_len(
+			joined_token->value_computed,
+			curr_token->value_computed,
+			cks_len(curr_token->value_computed)
+		);
+		joined_token->value_orig = cks_append_len(
+			joined_token->value_orig,
+			curr_token->value_orig,
+			cks_len(curr_token->value_orig)
+		);
+		item = item->next;
+	}
+	ckl_free_items(nam->tokens, am_free_token_content);
+	ckl_append(nam->tokens, joined_token);
+}
+
 int		am_finder_automaton(t_am *am, t_am_maker maker)
 {
 	t_am		*nam;
 	t_ckl_item	*item;
 	t_am_token	joined_token;
-	t_am_token	*curr_token;
 
 	nam = maker();
 	nam->source = cks_dup(am->source);
@@ -28,29 +55,7 @@ int		am_finder_automaton(t_am *am, t_am_maker maker)
 	if (am_ok(nam))
 	{
 		if (am->curr_finder->join)
-		{
-			joined_token.value_computed = cks_new("");
-			joined_token.value_orig = cks_new("");
-			joined_token.type = am->curr_finder->token_type;
-			item = nam->tokens->first;
-			while (item)
-			{
-				curr_token = &ckl_data(t_am_token, item);
-				joined_token.value_computed = cks_append_len(
-					joined_token.value_computed,
-					curr_token->value_computed,
-					cks_len(curr_token->value_computed)
-				);
-				joined_token.value_orig = cks_append_len(
-					joined_token.value_orig,
-					curr_token->value_orig,
-					cks_len(curr_token->value_orig)
-				);
-				item = item->next;
-			}
-			ckl_free_items(nam->tokens, am_free_token_content);
-			ckl_append(nam->tokens, &joined_token);
-		}
+			st_join_tokens(am, nam, &joined_token);
 		item = nam->tokens->first;
 		while (item)
 		{
