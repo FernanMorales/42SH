c2335e90e6d3d254c847910c048cef94cede6dfb
Conrad KLEINESPEL <ckleines@e1r6p21.42.fr>
2014-03-25 17:56:44 +0100

    fix

--- launcher/src/sh_builtin_env.c
+++ launcher/src/sh_builtin_env.c
@@ -2,7 +2,34 @@
 
 int		sh_builtin_env(t_sh_env *env, t_sh_command *cmd)
 {
-	ms_environ_print(environ);
+	t_ckl_item		*item;
+	char			*pos;
+
+	if (!cmd->argv || !cmd->argv->first)
+		return (1);
+	if (fork() == 0)
+	{
+		item = cmd->argv->first->next;
+		if (item)
+		{
+			if (cks_cmp(ckl_data(t_cks, cmd->argv->first->next), "-i") == 0)
+			{
+				ms_environ_clear();
+				item = item->next;
+			}
+			while (item)
+			{
+				if ((pos = ckstd_strchr(ckl_data(t_cks, item), '-')))
+				{
+					
+				}
+				item = item->next;
+			}
+		}
+		ms_environ_print(environ);
+		exit(0);
+	}
+	wait(NULL);
 	return (0);
 	(void)env;
 	(void)cmd;
