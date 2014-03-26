c2bd8e729cb868061e09e39739520ab66e9542fc
Conrad Kleinespel <conradk@conradk.com>
2014-03-26 18:24:01 +0100

    remove termcap prep

--- launcher/src/main.c
+++ launcher/src/main.c
@@ -24,26 +24,16 @@ int			main(int argc, const char **argv)
 	if (sh_init_env(&env, argc, argv) == 0)
 	{
 		g_sh_env = &env;
-		printf("%s\n", sh_read_line(&env));
-		return (0);
 		while ((sh_prompt(&env), (line = cks_get_line(0))) != NULL)
 		{
 			tree = ckbt_new(t_sh_command);
 			error = sh_parse(line, tree);
-			//ckbt_debug(tree, debug_tree);
 			if (!error)
 				sh_exec(&env, tree);
 			else
 				printf("42sh: syntax error\n");
 			cks_free(line);
 		}
-		/*
-		tree = ckbt_new(t_sh_command);
-		// pour les termcaps, quelque chose du genre ceci,
-		// qui boucle sur sh_parse tant que le retour != 0
-		// sh_parse((const char *)chaine, tree, (const char *)env->base);
-		char *cmd = termcap(&env, tree);
-		*/
 	}
 	return (0);
 }
--- launcher/src/sh_read_line.c
+++ launcher/src/sh_read_line.c
@@ -1,13 +1,26 @@
 #include "sh42.h"
 
+void		sh_print_line(t_sh_env *env, int print_cursor)
+{
+	t_ckl_item	*it;
+
+	tputs(tgetstr("dl", NULL), 0, sh_putc);
+	tputs(tgetstr("al", NULL), 0, sh_putc);
+	tputs(tgetstr("cr", NULL), 0, sh_putc);
+	sh_prompt(env);
+	it = env->char_list->first;
+	print_cursor = 0;
+}
+
 t_cks		sh_read_line(t_sh_env *env)
 {
 	int		status;
 	int		key;
 
+	if ((env->char_list = ckl_new(char)) == NULL)
+		return (NULL);
 	tputs(tgetstr("vi", NULL), 0, sh_putc);
 	tcsetattr(0, TCSADRAIN, &env->term_new);
-	sh_prompt(env);
 	while ((status = read(env->tty_fd, env->buf, SH_COMMAND_BUF_SIZE - 1)) > 0)
 	{
 		env->buf[status] = '\0';
